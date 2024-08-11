333d16feee9145f4aa0557ab7ef65675bd74e79d
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-08-11 11:27:00 +0500

    upadate all

--- app.js
+++ app.js
@@ -1,35 +1,20 @@
-//  var a=['bat','tap','cat','tap','pat']
-[['bat,tab'],['tap','pat'],['cat']]
 
-// frihtriojyfnfiytrlu,fdhjydfmjldfhjdfhijfjjeujykkd
-// fejhfjnjulnn
 
-let query = ["a", "nark", "bs", "hack", "stair"]
-let dictionary = ['hack', 'a', 'rank', 'khac', 'ackh', 'kran', 'rankhacker', 'a', 'ab', 'ba', 'stairs', 'raits']
+function myFunction(words) {
+    const arrWord = {};
 
-function sortArray(array) {
-    let answer = [];
-    for(let i = 0; i< array.length ; i++) {
-         let data = array[i].split('').sort().join('');
-         answer.push(data);
-    }
-    return answer;
+    words.forEach(word => {
+        const sortWord = word.split('').sort().join('');
+        if (arrWord[sortWord]) {
+            arrWord[sortWord].push(word);
+        } else {
+            arrWord[sortWord] = [word];
+        }
+    });
+
+    return Object.values(arrWord);
 }
 
-function stringAnagram(dictionary, query) {
-    let sortedDict = sortArray(dictionary);
-    let sortedQuery = sortArray(query);
-    let answer = [];
-    console.log(sortedDict.length);
-    console.log(sortedQuery.length);
-    sortedQuery.map(data => {
-        let i = 0;
-        sortedDict.forEach(dictData => {
-            if(data === dictData)
-                i++;
-        })
-        answer.push(i);
-    })
-
-    return answer;
-}
+const a = ["bat", "tap", "cat", "tab", "pat"];
+const b = myFunction(a);
+console.log(b);
